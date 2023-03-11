158759e6d6d19abd05174c194cf8a07ae75105d7
Elena <volshebnaya2393.gmail.com>
2023-03-11 17:28:05 +0500

    Написала код

---
+++ Example/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+string[] array;
+Console.WriteLine("Введите массив строк через пробел");
+string stroka = Console.ReadLine();
+array = stroka.Split(' ');
+var result = new string[array.Length];
+var realSize = 0;
+foreach (var value in array)
+{
+    if (value.Length <= 3)
+    {
+        result[realSize] = value;
+        realSize++;
+    }
+}
+Console.WriteLine("Массив с длинной значений 3 и меньше символов:");
+for (int i = 0; i < realSize; i++)
+{
+    Console.Write($"{result[i]} ");
+}
